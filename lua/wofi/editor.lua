5e4e298d7c0093d19b6393018328416153e8eb2b
Wilmer <wofinoli@gmail.com>
2023-06-27 09:42:51 -0500

    Update editor settings

--- lua/wofi/editor.lua
+++ lua/wofi/editor.lua
@@ -10,7 +10,16 @@ vim.opt.shiftwidth = 0 -- Sets < and > To move the same length as tab stop
 
 -- Search settings
 vim.opt.incsearch = true
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 
 -- Start without folds
 vim.opt.foldlevelstart = 99
+
+-- colors
+vim.opt.termguicolors = true
+
+-- Keep some lines at the end
+vim.opt.scrolloff = 8
+
+-- Update time
+vim.opt.updatetime = 50
--- lua/wofi/remap.lua
+++ lua/wofi/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
-vim.keymap.set("n", "<leader><leader>", ":bu #<cr>")
 
+vim.keymap.set("n", "<leader><leader>", ":bu #<cr>")
 vim.keymap.set("n", "<leader>b", ":buffers<cr>:buffer ")
+
